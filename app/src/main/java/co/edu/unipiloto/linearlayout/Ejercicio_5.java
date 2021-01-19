6c42957ba67e4d8fed930f9731e52ea48379fe27
Diego Manrique <damanrique@outlook.com>
2021-01-18 21:46:23 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/linearlayout/Ejercicio_5.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ejercicio_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio_5);
+    }
+}
---
+++ app/src/main/java/co/edu/unipiloto/linearlayout/Ejercicio_6.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ejercicio_6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio_6);
+    }
+}
